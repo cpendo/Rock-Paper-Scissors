e37b491bc6f5c6fc90f992f8717696050dbda23e
Cynthia Pendo <pendoc15@gmail.com>
2022-07-12 18:13:11 +0300

    Created a game function in .js file
    The game function allows the player a single round of 5 plays with the game stats and winner declared at the end of each round.
    The index and css files  have also been modified to display the round results in a table.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const names = ['Rock', 'Paper', 'Scissors'];
-const para = document.querySelector('p');
 const heading = document.querySelector('h4');
+const computer = document.querySelector('.computer')
+const player = document.querySelector('.player')
 
-function computerPlay(){
+
+function computerPlay() {
   const randomNumber = Math.floor(Math.random() * names.length);
   const choice = names[randomNumber];
   return choice;
@@ -12,33 +14,61 @@ function playRound(playerSelection, computerSelection) {
 
   playerSelection = prompt("Rock, Paper, Scissors");
   computerSelection = computerPlay();
-  heading.textContent = ` Computer: ${computerSelection} Player: ${playerSelection}`;
+  
+  alert(` Computer: ${computerSelection}   Player: ${playerSelection}`);
 
-  if (playerSelection === 'Rock' && computerSelection === 'Rock'){
+  if (playerSelection === 'Rock' && computerSelection === 'Rock') {
     return 'This is a tie!';
-  } else if (playerSelection === 'Paper' && computerSelection === 'Paper'){
+  } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
     return 'This is a tie!';
-  } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors'){
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
     return 'This is a tie!'
-  } else if (playerSelection === 'Rock' && computerSelection === 'Paper'){
+  } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
     return 'You lose! Paper beats Rock';
-  } else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
+  } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
     return 'You Win! Paper beats Rock';
-  } else if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
+  } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
     return 'You Win! Rock beats Scissors';
-  } else if (playerSelection === 'Scissors' && computerSelection === 'Rock'){
-      return 'You lose! Rock beats Scissors';
-  } else if (playerSelection === 'Paper' && computerSelection === 'Scissors'){
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+    return 'You lose! Rock beats Scissors';
+  } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
     return 'You lose! Scissors beats Paper';
-  } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
     return 'You win! Scissors beats Paper';
-  } 
+  }
   else {
-  return 'Boo Bitch!';
-    }
-    
-  } 
+    return 'Boo Bitch!';
+  }
+}
 
+let playerScore = 0;
 
+let computerScore = 0;
 
-para.textContent = playRound();
+function game() {
+
+  for (let i = 0; i < 5; i++) 
+  { result = playRound(); 
+    if (result === 'You lose! Paper beats Rock' ||
+        result === 'You lose! Scissors beats Paper' ||
+        result === 'You lose! Rock beats Scissors') {
+      computerScore ++;
+    } else if ( result === 'You Win! Paper beats Rock' ||
+        result === 'You Win! Rock beats Scissors' ||
+        result === 'You win! Scissors beats Paper') {
+      playerScore ++;
+    } 
+  }
+  player.textContent = playerScore;
+  computer.textContent = computerScore;
+
+  if (playerScore > computerScore){
+    return 'You Win!';
+  } else if (playerScore === computerScore){
+    return 'Its a Draw!';
+  } else {
+    return 'You Lose!';
+  }
+}
+
+heading.textContent = game();
