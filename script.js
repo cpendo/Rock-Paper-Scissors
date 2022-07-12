bc74d04e2398f7ece3cf3e291bd93b4153c80838
Cynthia Pendo <pendoc15@gmail.com>
2022-07-12 12:38:36 +0300

    Initial Commit
    The game prompts user for input and compares it  with the computers random choice to display appropriate response.
    Case Sensitive and runs on an endless loop.

---
+++ script.js
@@ -0,0 +1,42 @@
+const names = ['Rock', 'Paper', 'Scissors'];
+const para = document.querySelector('p');
+const heading = document.querySelector('h1');
+
+function computerPlay(){
+  const randomNumber = Math.floor(Math.random() * names.length);
+  const choice = names[randomNumber];
+  return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+
+  playerSelection = prompt("Rock, Paper, Scissors");
+  computerSelection = computerPlay();
+  para.textContent = ` Computer: ${computerSelection} Player: ${playerSelection}`;
+
+  if (playerSelection === 'Rock' && computerSelection === 'Rock'){
+    return 'This is a tie!';
+  } else if (playerSelection === 'Paper' && computerSelection === 'Paper'){
+    return 'This is a tie!';
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors'){
+    return 'This is a tie!'
+  } else if (playerSelection === 'Rock' && computerSelection === 'Paper'){
+      return 'You lose! Paper beats Rock';
+  } else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
+      return 'You Win! Paper beats Rock';
+  } else if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
+      return 'You Win! Rock beats Scissors';
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Rock'){
+      return 'You lose! Rock beats Scissors';
+  } else if (playerSelection === 'Paper' && computerSelection === 'Scissors'){
+      return 'You lose! Scissors beats Paper';
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
+      return 'You win! Scissors beats Paper';
+  } 
+  else {
+    return 'Boo Bitch!';
+  } 
+}
+
+
+alert(game());
